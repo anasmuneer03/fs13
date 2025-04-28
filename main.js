8af68f26d1b1543911ea8a7e1c7add9ef19cd71b
anasmuneer03 <anasqasem656@gmail.com>
2025-04-29 00:38:45 +0300

    user data to table/js task

---
+++ main.js
@@ -0,0 +1,11 @@
+var user = {
+  name: "tariq",
+  age: 31,
+  email: "tariq@gmail.com",
+  city: "qalqilia"
+};
+
+document.getElementById("name").textContent = user.name;
+document.getElementById("age").textContent = user.age;
+document.getElementById("email").textContent = user.email;
+document.getElementById("city").textContent = user.city;
